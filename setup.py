a497bc20229dafbb33e30d5030429b08402e1397
Ben Cho <ben.cho@gmail.com>
2020-05-23 16:45:35 -0600

    changed to beta

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setuptools.setup(
     ],
 	include_package_data=True,
 	classifiers=[
-		'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+		'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
 		'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
 		"Programming Language :: Python :: 3",
 		"Operating System :: OS Independent",
