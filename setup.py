6dbd151f0a7bc9e7b53fe63585385e5b52ccbbcf
Ben Cho <ben.cho@gmail.com>
2020-05-22 21:49:14 -0600

    updated setup.py with download_url

--- setup.py
+++ setup.py
@@ -7,12 +7,16 @@ setuptools.setup(
 	name="pyRTL",
 	version="0.0.1",
 	author="Ben Cho",
+ 	license='gpl-3.0',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
 	author_email="ben.cho@gmail.com",
 	description="Python wrapper for the R package RTL",
 	long_description=long_description,
 	long_description_content_type="text/markdown",
 	packages=setuptools.find_packages(),
 	package_data={'': ['*.csv']},
+	keywords = ['RTL', 'Risk', 'Tools', 'Trading', 'Crude', 'Oil'],
+	url = "https://github.com/bbcho/pyRTL-dev",
+	download_url = "https://github.com/bbcho/pyRTL-dev/archive/v0.0.1-beta.1.tar.gz",
 	install_requires=[
           'pandas',
           'numpy',
@@ -23,6 +27,8 @@ setuptools.setup(
     ],
 	include_package_data=True,
 	classifiers=[
+		'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+		'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
 		"Programming Language :: Python :: 3",
 		"Operating System :: OS Independent",
 	],
