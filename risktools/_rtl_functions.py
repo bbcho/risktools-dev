059820e3bcee1c4e9d749250c896d41811a09499
Ben Cho <ben.cho@gmail.com>
2020-11-11 00:52:56 -0700

    got performance analytics functions working

--- risktools/_rtl_functions.py
+++ risktools/_rtl_functions.py
@@ -544,6 +544,11 @@ def return_annualized(r, scale=None, geometric=True):
     -------
     floating point number if r is a Series, or pandas Series if r is a DataFrame
 
+    References
+    ----------
+    Bacon, Carl. \emph{Practical Portfolio Performance Measurement
+    and Attribution}. Wiley. 2004. p. 6
+
     Examples
     --------
     >>> from pandas_datareader import data, wb
@@ -556,25 +561,7 @@ def return_annualized(r, scale=None, geometric=True):
     >>> return_annualized(r=df['Adj Close'], geometric=True)
     >>> return_annualized(r=df['Adj Close'], geometric=False)
     """
-    if ~(isinstance(r, pd.DataFrame) | isinstance(r,pd.Series)):
-        raise ValueError("r must be a pandas Series or DataFrame")
-
-    if isinstance(r.index, pd.DatetimeIndex):
-        if scale is None:
-            if (r.index.freq == 'D') | (r.index.freq == 'B'):
-                scale = 252
-            elif r.index.freq == 'W':
-                scale = 52
-            elif (r.index.freq == 'M') | (r.index.freq == 'MS'):
-                scale = 12
-            elif (r.index.freq == 'Q') | (r.index.freq == 'QS'):
-                scale = 4
-            elif (r.index.freq == 'Y') | (r.index.freq == 'YS'):
-                scale = 1
-            else:
-                raise ValueError("parameter r's index must be a datetime index with freq 'D','W','M','Q' or 'Y'")
-    else:
-        raise ValueError("parameter r's index must be a datetime index with freq 'D','W','M','Q' or 'Y'")
+    r, scale = _check_ts(r,scale, name='r')
     
     r = r.dropna()
     n = r.shape[0]
@@ -583,6 +570,58 @@ def return_annualized(r, scale=None, geometric=True):
         res = (r.add(1).cumprod()**(scale/n)-1).iloc[-1]
     else:
         res = r.mean()*scale
+    return res
+
+
+def return_excess(R, Rf=0):
+    """
+    Calculates the returns of an asset in excess of the given risk free rate
+    
+    Calculates the returns of an asset in excess of the given "risk free rate"
+    for the period.
+    
+    Ideally, your risk free rate will be for each period you have returns
+    observations, but a single average return for the period will work too.
+    
+    Mean of the period return minus the period risk free rate
+    
+    \deqn{\overline{(R_{a}-R_{f})}}{mean(Ra-Rf=0)}
+    
+    OR
+    
+    mean of the period returns minus a single numeric risk free rate
+    
+    \deqn{\overline{R_{a}}-R_{f}}{mean(R)-rf}
+    
+    Note that while we have, in keeping with common academic usage, assumed that
+    the second parameter will be a risk free rate, you may also use any other
+    timeseries as the second argument.  A common alteration would be to use a
+    benchmark to produce excess returns over a specific benchmark, as
+    demonstrated in the examples below.
+
+    Parameters
+    ----------
+    R : Series or DataFrame
+        A time series of asset returns
+    Rf : float
+        risk free rate, in same period as your returns, or as a single
+        digit average
+    
+    References
+    ----------
+    Bacon, Carl. \emph{Practical Portfolio Performance Measurement
+    and Attribution}. Wiley. 2004. p. 47-52
+    
+    Examples
+    --------
+    data(managers)
+    head(Return.excess(managers[,1,drop=FALSE], managers[,10,drop=FALSE]))
+    head(Return.excess(managers[,1,drop=FALSE], .04/12))
+    head(Return.excess(managers[,1:6], managers[,10,drop=FALSE]))
+    head(Return.excess(managers[,1,drop=FALSE], managers[,8,drop=FALSE]))
+    """
+
+    res = R - Rf
     return res
 
 
@@ -686,7 +725,7 @@ def omega_sharpe_ratio(R, MAR, *args):
     ----------
     R : Series or DataFrame
         A time series of asset returns
-    MAR : int
+    MAR : int or array_like
         Minimum Acceptable Return, in the same periodicity as your
         returns
     *args:
@@ -700,6 +739,29 @@ def omega_sharpe_ratio(R, MAR, *args):
     >>> mar = 0.005
     >>> print(omega_sharpe_ratio(portfolio_bacon[,1], MAR))
     """
+    if isinstance(R, (pd.Series, pd.DataFrame)):
+        if isinstance(R.index, pd.DatetimeIndex) & isinstance(MAR, (pd.Series, pd.DataFrame)):
+            if ~isinstance(MAR.index, pd.DatetimeIndex):
+                raise ValueError("MAR index must be a datatime index if MAR and R are a Dataframe or Series with a datetime index")
+        elif ~isinstance(R.index, pd.DatetimeIndex) & isinstance(MAR, (pd.Series, pd.DataFrame)):
+            if isinstance(MAR.index, pd.DatetimeIndex):
+                raise ValueError("R does not have a datetime index but MAR does. If both DataFrames or Series, index types must be the same")
+
+    R = R.dropna()
+    r = R[R.gt(MAR)]
+
+    if isinstance(MAR, (pd.Series, pd.DataFrame)) & isinstance(R, (pd.Series, pd.DataFrame)):
+        # subset to the same dates as the R data. Already checked that if both are series or dataframes, that
+        # indices are of the same type
+        MAR = MAR[r.index]
+    else:
+        # works for any array_like MAR. Scalars will just return itself
+        # if MAR is array_like, we have to assume that R and MAR both 
+        # cover the same time period
+        MAR = np.mean(MAR)
+
+    result = (upside_risk(R, MAR, stat='potential') - downside_deviation(R, MAR, potential=True)) / downside_deviation(R, MAR, potential=True)
+    return result
 
 
 def upside_risk(R, MAR=0, method='full', stat='risk'):
@@ -746,17 +808,15 @@ def upside_risk(R, MAR=0, method='full', stat='risk'):
     
     Examples
     --------
-
-    #' data(portfolio_bacon)
-    #' MAR = 0.005
-    #' print(UpsideRisk(portfolio_bacon[,1], MAR, stat="risk")) #expected 0.02937
-    #' print(UpsideRisk(portfolio_bacon[,1], MAR, stat="variance")) #expected 0.08628
-    #' print(UpsideRisk(portfolio_bacon[,1], MAR, stat="potential")) #expected 0.01771
-    #'
-    #' MAR = 0
-    #' data(managers)
-    #' print(UpsideRisk(managers['1996'], MAR, stat="risk"))
-    #' print(UpsideRisk(managers['1996',1], MAR, stat="risk")) #expected 1.820
+    >>> from pandas_datareader import data, wb
+    >>> from datetime import datetime
+    >>> df = data.DataReader(["SPY","AAPL"],  "yahoo", datetime(2000,1,1), datetime(2012,1,1))
+    >>> df = df.pct_change()
+    >>> df = df.asfreq('B')
+    >>> print(upside_risk(df['Adj Close'], MAR=0))
+    >>> print(upside_risk(df[('Adj Close','SPY')], MAR=0))
+    >>> print(upside_risk(df['Adj Close'], MAR=0, stat='variance'))
+    >>> print(upside_risk(df['Adj Close'], MAR=0, stat='potential'))
     """
 
     if isinstance(R, (pd.Series, pd.DataFrame)):
@@ -786,7 +846,7 @@ def upside_risk(R, MAR=0, method='full', stat='risk'):
         length = len(r)
 
     if stat == 'risk':
-        result = r.sub(MAR).pow(2).div(length).sum().apply(np.sqrt)
+        result = np.sqrt(r.sub(MAR).pow(2).div(length).sum())
     elif stat == 'variance':
         result = r.sub(MAR).pow(2).div(length).sum()
     else:
@@ -824,17 +884,13 @@ def downside_deviation(R, MAR=0, method='full', potential=False):
     
     Examples
     --------
-
-    #' data(portfolio_bacon)
-    #' MAR = 0.005
-    #' print(UpsideRisk(portfolio_bacon[,1], MAR, stat="risk")) #expected 0.02937
-    #' print(UpsideRisk(portfolio_bacon[,1], MAR, stat="variance")) #expected 0.08628
-    #' print(UpsideRisk(portfolio_bacon[,1], MAR, stat="potential")) #expected 0.01771
-    #'
-    #' MAR = 0
-    #' data(managers)
-    #' print(UpsideRisk(managers['1996'], MAR, stat="risk"))
-    #' print(UpsideRisk(managers['1996',1], MAR, stat="risk")) #expected 1.820
+    >>> from pandas_datareader import data, wb
+    >>> from datetime import datetime
+    >>> df = data.DataReader(["SPY","AAPL"],  "yahoo", datetime(2000,1,1), datetime(2012,1,1))
+    >>> df = df.pct_change()
+    >>> df = df.asfreq('B')
+    >>> print(downside_deviation(df[('Adj Close','SPY')], MAR=0))
+    >>> print(downside_deviation(df['Adj Close'], MAR=0))
     """
 
     if isinstance(R, (pd.Series, pd.DataFrame)):
@@ -864,11 +920,247 @@ def downside_deviation(R, MAR=0, method='full', potential=False):
         length = len(r)
 
     if potential:
-        result = -r.add(MAR).div(length).sum()
+        result = r.mul(-1).add(MAR).div(length).sum()
     else:
-        result = -r.add(MAR).pow(2).div(length).sum().apply(np.sqrt)    
+        result = np.sqrt(r.mul(-1).add(MAR).pow(2).div(length).sum())
+        # result = r.mul(-1).add(MAR).pow(2).div(length).sum().apply(np.sqrt)    
 
     return result
+
+
+def _check_ts(R, scale, name='R'):
+    """
+    Function to check frequency of R, a time series Series or Dataframe
+
+    Parameters
+    ----------
+    R : Series or DataFrame
+        A time series of asset returns
+    scale : int (optional)
+        number of periods in a year (daily scale = 252, monthly scale =
+        12, quarterly scale = 4). By default None. Note that if scale is None,
+        the function will calculate a scale based on the index frequency. If however
+        you wish to override this (becuase maybe there is no index freq), 
+        specify your own scale to use.
+
+    Returns
+    -------
+    tuple with R as Series or Dataframe and scale as int
+    """
+    if (~isinstance(R, pd.DataFrame) & ~isinstance(R, pd.Series)) == True:
+        raise ValueError(f"{name} must be a pandas Series or DataFrame")
+
+    if isinstance(R.index, pd.DatetimeIndex):
+        if scale is None:
+            if (R.index.freq == 'D') | (R.index.freq == 'B'):
+                scale = 252
+            elif R.index.freq == 'W':
+                scale = 52
+            elif (R.index.freq == 'M') | (R.index.freq == 'MS'):
+                scale = 12
+            elif (R.index.freq == 'Q') | (R.index.freq == 'QS'):
+                scale = 4
+            elif (R.index.freq == 'Y') | (R.index.freq == 'YS'):
+                scale = 1
+            else:
+                raise ValueError(f"parameter {name}'s index must be a datetime index with freq 'D','B','W','M','Q' or 'Y'")
+    else:
+        raise ValueError(f"parameter {name}'s index must be a datetime index with freq 'D','B','W','M','Q' or 'Y'")
+
+    return R, scale
+
+
+def sharpe_ratio_annualized(R, Rf=0, scale=None, geometric=True):
+    """
+    calculate annualized Sharpe Ratio
+    
+    The Sharpe Ratio is a risk-adjusted measure of return that uses standard
+    deviation to represent risk.
+    
+    The Sharpe ratio is simply the return per unit of risk (represented by
+    variance).  The higher the Sharpe ratio, the better the combined performance
+    of "risk" and return.
+    
+    This function annualizes the number based on the scale parameter.
+    
+    \deqn{\frac{\sqrt[n]{prod(1+R_{a})^{scale}}-1}{\sqrt{scale}\cdot\sqrt{\sigma}}}
+    
+    Using an annualized Sharpe Ratio is useful for comparison of multiple return
+    streams.  The annualized Sharpe ratio is computed by dividing the annualized
+    mean monthly excess return by the annualized monthly standard deviation of
+    excess return.
+    
+    William Sharpe now recommends Information Ratio preferentially to the
+    original Sharpe Ratio.
+
+    Parameters
+    ----------
+    R : array_like
+        an xts, vector, matrix, data frame, timeSeries or zoo object of
+        asset returns
+    Rf : float
+        risk free rate, in same period as your returns. By default 0
+    scale : int
+        number of periods in a year (daily scale = 252, monthly scale =
+        12, quarterly scale = 4). By default None
+    geometric : bool
+        utilize geometric chaining (True) or simple/arithmetic chaining (False) to aggregate returns,
+        default True
+    
+    see also \code{\link{SharpeRatio}} \cr \code{\link{InformationRatio}} \cr
+    \code{\link{TrackingError}} \cr \code{\link{ActivePremium}} \cr
+    \code{\link{SortinoRatio}}
+    
+    References
+    ----------
+    Sharpe, W.F. The Sharpe Ratio,\emph{Journal of Portfolio
+    Management},Fall 1994, 49-58.
+    
+    Examples
+    --------
+    >>> from pandas_datareader import data, wb
+    >>> from datetime import datetime
+    >>> df = data.DataReader(["SPY","AAPL"],  "yahoo", datetime(2000,1,1), datetime(2012,1,1))
+    >>> df = df.pct_change()
+    >>> df = df.asfreq('B')
+
+    data(managers)
+    SharpeRatio.annualized(managers[,1,drop=FALSE], Rf=.035/12) 
+    SharpeRatio.annualized(managers[,1,drop=FALSE], Rf = managers[,10,drop=FALSE])
+    SharpeRatio.annualized(managers[,1:6], Rf=.035/12) 
+    SharpeRatio.annualized(managers[,1:6], Rf = managers[,10,drop=FALSE])
+    SharpeRatio.annualized(managers[,1:6], Rf = managers[,10,drop=FALSE],geometric=FALSE)
+    """
+    R, scale = _check_ts(R, scale)
+
+    xR = return_excess(R, Rf)
+    res = return_annualized(xR, scale=scale, geometric=geometric)
+    res /= sd_annualized(R, scale=scale)
+
+    return res
+
+
+def drawdowns(R, geometric=True, *args):
+    """
+    Function to calculate drawdown levels in a timeseries
+
+    Parameters
+    ----------
+    R : Series or DataFrame
+        pandas Series or DataFrame with a datetime index. If DataFrame, function will calculate 
+        cummmulative returns on each column
+    geometric : bool
+        utilize geometric chaining (TRUE) or simple/arithmetic chaining (FALSE) to aggregate returns, by default True
+
+    """
+
+    if geometric:
+        res = R.add(1).cumprod()
+    else:
+        res = R.cumsum()+1
+
+    res = res/res.clip(lower=1).cummax()-1
+
+    return res
+
+
+def find_drawdowns(R, geometric=True, *args):
+    """
+    Find the drawdowns and drawdown levels in a timeseries.
+    
+    find_drawdowns() will find the starting period, the ending period, and
+    the amount and length of the drawdown.
+    
+    Often used with sort_drawdowns() to get the largest drawdowns.
+    
+    drawdowns() will calculate the drawdown levels as percentages, for use
+    in \code{\link{chart.Drawdown}}.
+    
+    Returns an dictionary: \cr 
+    \itemize{ 
+      \item return depth of drawdown
+      \item from starting period 
+      \item to ending period \item length length in periods 
+    }
+    
+    Parameters
+    ----------
+    R : Series or DataFrame
+        pandas Series or DataFrame with a datetime index. If DataFrame, function will calculate 
+        cummmulative returns on each column
+    geometric : bool
+        utilize geometric chaining (TRUE) or simple/arithmetic chaining (FALSE) to aggregate returns, by default True
+    *args
+        any pass-through parameters
+
+    References
+    ----------
+    Bacon, C. \emph{Practical Portfolio Performance Measurement and
+    Attribution}. Wiley. 2004. p. 88 \cr
+    
+    Examples
+    --------
+    data(edhec)
+    findDrawdowns(edhec[,"Funds of Funds", drop=FALSE])
+    sortDrawdowns(findDrawdowns(edhec[,"Funds of Funds", drop=FALSE]))
+    """
+    dd = drawdowns(R, geometric=geometric).dropna()
+
+    # init result dict
+    rs = dict()
+
+    for lab, con in dd.iteritems():
+        rs[lab] = dict()
+        rs[lab]['return'] = np.array([]).astype(float)
+        rs[lab]['from'] = np.array([]).astype(int)
+        rs[lab]['to'] = np.array([]).astype(int)
+        rs[lab]['length'] = np.array([]).astype(int)
+        rs[lab]['trough'] = np.array([]).astype(int)
+
+        if con[0] >= 0:
+            prior_sign = 1
+        else:
+            prior_sign = 0
+
+        frm = 0
+        to = 0
+        dmin = 0
+        sofar = con[0]
+
+        for i, r in enumerate(con): #.iteritems():
+            if r < 0:
+                this_sign = 0
+            else:
+                this_sign = 1
+
+            if this_sign == prior_sign:
+                if r < sofar:
+                    sofar = r
+                    dmin = i
+                to = i+1
+            else:
+                rs[lab]['return'] = np.append(rs[lab]['return'],sofar)
+                rs[lab]['from'] = np.append(rs[lab]['from'],frm)
+                rs[lab]['trough'] = np.append(rs[lab]['trough'],dmin)
+                rs[lab]['to'] = np.append(rs[lab]['to'],to)
+
+                frm = i
+                sofar = r
+                to = i+1
+                dmin = i
+                prior_sign = this_sign
+
+        rs[lab]['return'] = np.append(rs[lab]['return'],sofar)
+        rs[lab]['from'] = np.append(rs[lab]['from'],frm)
+        rs[lab]['trough'] = np.append(rs[lab]['trough'],dmin)
+        rs[lab]['to'] = np.append(rs[lab]['to'],to)
+
+        rs[lab]['length'] = rs[lab]['to'] - rs[lab]['from'] + 1
+        rs[lab]['peaktrough'] = rs[lab]['trough'] - rs[lab]['from'] + 1
+        rs[lab]['recovery'] = rs[lab]['to'] - rs[lab]['trough']
+    return rs
+
+
 
 
 if __name__ == "__main__":
@@ -881,21 +1173,34 @@ if __name__ == "__main__":
     # print(bond(output='price'))
     # print(bond(output='duration'))
     # print(bond(output='df'))
-
     
     from pandas_datareader import data, wb
     from datetime import datetime
     df = data.DataReader(["SPY","AAPL"],  "yahoo", datetime(2000,1,1), datetime(2012,1,1))
     df = df.pct_change()
     df = df.asfreq('B')
+
+    # tt = _sr(df['Adj Close'], Rf=0, scale=252)
+    # print(tt)
+
+    # print(drawdowns(df['Adj Close']))
+    # print(drawdowns(df[('Adj Close','SPY')]))
+
+    rs = find_drawdowns(df['Adj Close'])
+    print(rs['SPY']['return'])
+    # print(find_drawdowns(df[('Adj Close','SPY')]))
+
+    # print(sharpe_ratio_annualized(df['Adj Close']))
+    # print(sharpe_ratio_annualized(df[('Adj Close','SPY')]))
     
-    print(upside_risk(df['Adj Close'], MAR=0))
+    # print(omega_sharpe_ratio(df['Adj Close'],MAR=0))
+    # print(upside_risk(df['Adj Close'], MAR=0))
+    # print(upside_risk(df[('Adj Close','SPY')], MAR=0))
     # print(upside_risk(df['Adj Close'], MAR=0, stat='variance'))
     # print(upside_risk(df['Adj Close'], MAR=0, stat='potential'))
 
-    print(downside_deviation(df['Adj Close'], MAR=0))
-
-    # print(df.tail())
+    # print(downside_deviation(df[('Adj Close','SPY')], MAR=0))
+    # print(downside_deviation(df['Adj Close'], MAR=0))
     
     # print(return_cumulative(r=df['Adj Close'], geometric=True))
     # print(return_cumulative(r=df['Adj Close'], geometric=False))
@@ -908,3 +1213,15 @@ if __name__ == "__main__":
 
     # print(sd_annualized(x=df[('Adj Close','SPY')]))
     # print(sd_annualized(x=df['Adj Close']))
+
+    ####################
+    # R code for testing
+    # library(timetk)
+    # library(tidyverse)
+    # library(PerformanceAnalytics)
+    # df <- tq_get('SPY', from='2000-01-01', to='2012-01-01')
+    # df <- df %>% dplyr::mutate(adjusted = adjusted/lag(adjusted)-1)
+    # R = tk_xts(df, select=adjusted, date_var=date)
+    # DownsideDeviation(R, MAR=0)
+    # UpsideRisk(R, 0)
+    # OmegaSharpeRatio(R, 0)
