076c116da78e809633ea48d65a592d7202420561
Ben Cho <ben.cho@gmail.com>
2021-12-24 23:56:26 -0700

    added tests for 3.7, 3.10

--- setup.py
+++ setup.py
@@ -49,5 +49,5 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.6",
+    python_requires=">=3.7",
 )
