565deadddc844c360a41ddcca4ff18c19996f16f
Ben Cho <ben.cho@gmail.com>
2022-12-03 22:28:14 -0700

    simOU opt in c

---
+++ src/risktools/c/simOU.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+// https://stackoverflow.com/questions/5862915/passing-numpy-arrays-to-a-c-function-for-input-and-output
+// https://stackoverflow.com/questions/4355524/getting-data-from-ctypes-array-into-numpy
+// cc -fPIC -shared -o simOU.so simOU.c
+
+void csimOU(
+    double *x,
+    const double theta,
+    const double *mu,
+    const double dt,
+    const double sigma,
+    const size_t rows,
+    const size_t cols)
+{
+    size_t i;
+    size_t j = 0;
+
+    // pre-compute to make faster
+    const double dt_theta = dt * theta;
+    const int ll = rows * cols;
+
+    // input x is a 2D array that has been reshaped to be 1D.
+    // Loop through entire 1D array of length rows*cols,
+    // skips value every time counter j is reaches row
+    // count - effectively a new sim.
+
+    for (i = 1; i < ll; ++i)
+    {
+        if (j >= rows - 1)
+        {
+            j = 0;
+        }
+        else
+        {
+            j = j + 1;
+            x[i] = x[i - 1] + (mu[i] - x[i - 1]) * dt_theta + sigma * x[i];
+        }
+    }
+    // return x;
+}
+
+// void cfun(const double *indatav, size_t size, double *outdatav)
+// {
+//     size_t i;
+//     for (i = 0; i < size; ++i)
+//         outdatav[i] = indatav[i] * 2.0;
+// }
