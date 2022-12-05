c427657831a037b9952984a9fc1f0466c9efad9c
Ben Cho <ben.cho@gmail.com>
2022-12-04 19:51:32 -0700

    simOU fastest cleanup

--- src/risktools/c/simOU.c
+++ src/risktools/c/simOU.c
@@ -4,6 +4,42 @@
 // https://stackoverflow.com/questions/4355524/getting-data-from-ctypes-array-into-numpy
 // cc -fPIC -shared -o simOU.so simOU.c
 
+void csimOU(
+    double *x,
+    const double theta,
+    const double *mu,
+    const double dt,
+    const double sigma,
+    const size_t rows,
+    const size_t cols)
+{
+    size_t i;
+    size_t j = 0;
+
+    // pre-compute to make faster
+    const double dt_theta = dt * theta;
+    const int ll = rows * cols;
+
+    // input x is a 2D array that has been reshaped to be 1D.
+    // Loop through entire 1D array of length rows*cols,
+    // skips value every time counter j is reaches row
+    // count - effectively a new sim.
+
+    for (i = 1; i < ll; ++i)
+    {
+        if (j >= cols - 1)
+        {
+            j = 0;
+        }
+        else
+        {
+            j = j + 1;
+            x[i] = x[i - 1] + (mu[i] - x[i - 1]) * dt_theta + sigma * x[i];
+        }
+    }
+    // return x;
+}
+
 // void csimOU(
 //     double *x,
 //     const double theta,
@@ -40,39 +76,3 @@
 //     for (i = 0; i < size; ++i)
 //         outdatav[i] = indatav[i] * 2.0;
 // }
-
-void csimOU(
-    double *x,
-    const double theta,
-    const double *mu,
-    const double dt,
-    const double sigma,
-    const size_t rows,
-    const size_t cols)
-{
-    size_t i;
-    size_t j = 0;
-
-    // pre-compute to make faster
-    const double dt_theta = dt * theta;
-    const int ll = rows * cols;
-
-    // input x is a 2D array that has been reshaped to be 1D.
-    // Loop through entire 1D array of length rows*cols,
-    // skips value every time counter j is reaches row
-    // count - effectively a new sim.
-
-    for (i = 1; i < ll; ++i)
-    {
-        if (j >= cols - 1)
-        {
-            j = 0;
-        }
-        else
-        {
-            j = j + 1;
-            x[i] = x[i - 1] + (mu[i] - x[i - 1]) * dt_theta + sigma * x[i];
-        }
-    }
-    // return x;
-}
