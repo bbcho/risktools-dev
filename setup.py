ba24b0755520f7870d1a802a6b83becbc804094d
Ben Cho <ben.cho@gmail.com>
2022-12-11 19:47:31 -0700

    build working for dll

--- setup.py
+++ setup.py
@@ -1,12 +1,31 @@
 import setuptools
 from setuptools import Extension
+import os
+import sysconfig
+from Cython.Distutils import build_ext
+
+
+class NoSuffixBuilder(build_ext):
+    def get_ext_filename(self, ext_name):
+        filename = super().get_ext_filename(ext_name)
+        suffix = sysconfig.get_config_var('EXT_SUFFIX')
+        ext = os.path.splitext(filename)[1]
+        return filename.replace(suffix, "") + ext
+
+extensions = [
+    Extension(
+    name="simOU", 
+    sources=["src/risktools/c/simOU.c"],
+    extra_compile_args=['-fPIC', '-shared']
+    )
+]
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="risktools",
-    version="0.2.4",
+    version="0.2.5",
     author="Ben Cho",
     license="gpl-3.0",  # Chose a license from here: https://help.github.com/articles/licensing-a-repository
     author_email="ben.cho@gmail.com",
@@ -55,4 +74,7 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.7",
+    # ext_package='risktools',
+    cmdclass={"build_ext": NoSuffixBuilder},
+    ext_modules = extensions,
 )
